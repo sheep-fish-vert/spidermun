307985546aef027489595dc2165eec78d683bb99
Dmitry <dmitry1939@gmail.com>
2016-05-26 09:56:22 +0300

    baraban markup

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,4 +1,21 @@
+/* baraban script */
 
+    function barabanScript(){
+
+        $.ajax({
+            url:'js/json/users_list_less.json', // ajaxurl  // js/json/user_list_more.json
+            data:'baraban-go',
+            method:'POST',
+            success:function(data){
+
+
+
+            }
+        });
+
+    }
+
+/* baraban script */
 
 
 $(document).ready(function(){
@@ -6,6 +23,8 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
+
+    barabanScript(); // remove after done Radiks work
 
 });
 
