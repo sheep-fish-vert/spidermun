fdae9d13069ac944176b894708746bb5a8333768
radiks <radion91@gmail.com>
2016-05-26 11:13:36 +0300

    no message

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,12 +1,48 @@
 var clock;
 
-
 function clockCountdown(){
-    clock = $('#main_clock').FlipClock(clockSecond, {
+    clock = $('#main_clock').FlipClock(20/*clockSecond*/, {
         countdown: true,
-        clockFace: 'MinuteCounter'
+        clockFace: 'MinuteCounter',
+        callbacks: {
+            interval: function () {
+                // Вызов каждую секунду
+            },
+            stop:function (){
+                // Вызов когда кончиться время
+                console.log('stop');
+                setTimeout(function(){
+                    animateSpidy();
+                },500);
+
+            },
+            reset:function (){
+                console.log('reset');
+            }
+        }
     });
 }
+
+
+function animateSpidy(){
+    var spidy = $('.spidy-wrap');
+    spidy.addClass('active');
+    setTimeout(function(){
+        console.log('animate is gone');
+        // function about baraban run
+    },3400);
+}
+
+
+
+function resetClockTimer(){
+    spidy.removeClass('active');
+    clock.setTime(20/*clockSecond*/);
+    clock.start();
+}
+
+
+
 
 
 $(document).ready(function(){
